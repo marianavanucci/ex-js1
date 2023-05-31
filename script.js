544db6f8dc8776a54cddba52883e664a3b512cfb
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-05-31 19:50:22 -0300

    evt.preventDefault bug fix

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// resposta aula 20
+
 console.log(`ola!`);
 
 function hello() {
-  alert(1);
   const form = document.querySelector(".form");
-  form.onsubmit = function (event) {
-    alert(2);
-  };
+
+  // form.onsubmit = function (evento) {
+  //   evento.preventDefaut();
+  //   alert(1);
+  //   console.log("foi enviado");
+  // };
+
+  function stopDefAction(evt) {
+    evt.preventDefault();
+    console.log("form nao foi enviado");
+    // event.preventDefault(); = Cancela o evento se for cancelável,
+    // sem parar a propagação do mesmo.
+  }
+
+  form.addEventListener("submit", stopDefAction);
 }
 
 hello();
