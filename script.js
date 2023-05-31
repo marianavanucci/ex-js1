cd0cbbcc4b7d10d9dfe3425ea518ef064e52ddcd
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-05-31 20:32:14 -0300

    pessoas push() + resultado

--- script.js
+++ script.js
@@ -2,23 +2,55 @@
 
 console.log(`ola!`);
 
-function hello() {
+// function hello() {
+//   const form = document.querySelector(".form");
+
+// form.onsubmit = function (evento) {
+//   evento.preventDefaut();
+//   alert(1);
+//   console.log("foi enviado");
+// };
+
+//   let contador = 1;
+
+//   function stopDefAction(evt) {
+//     evt.preventDefault();
+//     console.log(`form nao foi enviado ${contador}`);
+//     // event.preventDefault(); = Cancela o evento se for cancelável,
+//     // sem parar a propagação do mesmo.
+//     contador++;
+//   }
+
+//   form.addEventListener("submit", stopDefAction);
+// }
+
+// hello();
+
+function meuEscopo() {
   const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
 
-  // form.onsubmit = function (evento) {
-  //   evento.preventDefaut();
-  //   alert(1);
-  //   console.log("foi enviado");
-  // };
+  const pessoas = [];
 
-  function stopDefAction(evt) {
+  function recebeEventoForm(evt) {
     evt.preventDefault();
-    console.log("form nao foi enviado");
-    // event.preventDefault(); = Cancela o evento se for cancelável,
-    // sem parar a propagação do mesmo.
+
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const peso = form.querySelector(".peso");
+    const altura = form.querySelector(".altura");
+    // console.log(nome.value, sobrenome.value, peso.value, altura.value);
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      altura: altura.value,
+      peso: peso.value,
+    });
+    console.log(pessoas);
+    resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>`;
   }
 
-  form.addEventListener("submit", stopDefAction);
+  form.addEventListener("submit", recebeEventoForm);
 }
 
-hello();
+meuEscopo();
